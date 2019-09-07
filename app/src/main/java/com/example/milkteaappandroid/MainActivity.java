bd2199d6806a51931a2af7e505ae134ba5ada684
VSN2015 <sangnguyen07dhth@gmail.com>
2019-09-07 18:47:25 +0700

    VSN chỉnh sửa trang chủ, thêm fragment, khang lỗi sml

--- app/src/main/java/com/example/milkteaappandroid/MainActivity.java
+++ app/src/main/java/com/example/milkteaappandroid/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.milkteaappandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dangki_layout);
+        setContentView(R.layout.trangchu_layout);
+
+        /*include nav bottom in Home*/
+        bottomNavigationView =findViewById(R.id.bottom_nav_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        /* end include nav bottom in Home*/
+
+    }
+    /*các class fragment*/
+    Home homeFragment= new Home();
+    GioHang gioHangFragment= new GioHang();
+    DSSanPham dsSanPhamFragment= new DSSanPham();
+    ThongBao thongBaoFragment= new ThongBao();
+    TaiKhoan taiKhoanFragment = new TaiKhoan();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {  /*sự kiện chọn item nav*/
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerHome,homeFragment).commit();
+                return true;
+            case R.id.nav_cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerHome,gioHangFragment).commit();
+                return true;
+            case R.id.nav_product:
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerHome,dsSanPhamFragment).commit();
+                return true;
+            case R.id.nav_notification:
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerHome,thongBaoFragment).commit();
+                return true;
+            case R.id.nav_account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerHome,taiKhoanFragment).commit();
+                return true;
+
+        }
+        return false;
     }
 }
