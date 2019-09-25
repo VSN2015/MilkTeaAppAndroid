3bc853e53e5f3977e82752c1c9fd9511b7cffe48
NT2015 <46642030+NT2015@users.noreply.github.com>
2019-09-25 17:35:28 +0700

    Sửa file đăng kí

--- app/src/main/java/com/example/milkteaappandroid/DangKi_Activity.java
+++ app/src/main/java/com/example/milkteaappandroid/DangKi_Activity.java
@@ -38,12 +38,12 @@ public class DangKi_Activity extends AppCompatActivity implements View.OnClickLi
 
         // tìm các control dang ki ở ngoài giao diện
         btnDangKi = (Button) findViewById(R.id.btn_DangKi);
-        edHoTen = (EditText) findViewById(R.id.ed_HoTen_DK);
-        edDiaChi = (EditText) findViewById(R.id.ed_DiaChi_DK);
-        edSdt = (EditText) findViewById(R.id.ed_SDT_DK);
-        edEmail = (EditText) findViewById(R.id.ed_Email_DK);
-        edMatKhau = (EditText) findViewById(R.id.ed_MatKhau_DK);
-        edNhapLaiMK = (EditText) findViewById(R.id.ed_NhapLaiPass_DK);
+        edHoTen = findViewById(R.id.ed_HoTen_DK);
+        edDiaChi = findViewById(R.id.ed_DiaChi_DK);
+        edSdt = findViewById(R.id.ed_SDT_DK);
+        edEmail = findViewById(R.id.ed_Email_DK);
+        edMatKhau = findViewById(R.id.ed_MatKhau_DK);
+        edNhapLaiMK = findViewById(R.id.ed_NhapLaiPass_DK);
 
 
         // lắng nghe sự kiện click của button tại trang đăng kí
@@ -60,39 +60,46 @@ public class DangKi_Activity extends AppCompatActivity implements View.OnClickLi
         String email = edEmail.getText().toString();
         String matkhau = edMatKhau.getText().toString();
         String nhaplaimk = edNhapLaiMK.getText().toString();
+
         String thongbaoloinhap = getString(R.string.thongbaoloidangki);
         String thongbaomk = getString(R.string.thongbaonhaplaimk);
 
-        // kiểm tra điều kiện
-        if(hoten.trim().length() < 0){
-            thongbaoloinhap += getString(R.string.hoten);
-            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show(); // xuất thông báo bằng toast
-        }else if(diachi.length() < 10){
-            thongbaoloinhap += getString(R.string.diachi);
-            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
-        }else if(sdt.length() < 10 || sdt.length() > 10){
-            thongbaoloinhap += getString(R.string.sodt);
-            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
-        }else if(email.trim().length() < 0){
-            thongbaoloinhap += getString(R.string.email);
-            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
-        }else if(matkhau.trim().length() < 0){
-            thongbaoloinhap += getString(R.string.password);
-            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
-        }else if(!nhaplaimk.equals(matkhau)){
-            Toast.makeText(this, getString(R.string.thongbaonhaplaimk), Toast.LENGTH_SHORT).show();
-        }else // nhập đúng và đầy đủ thông tin
-        {
-            // gọi phương thức tạo tài khoảng bằng email và password
-            firebaseAuth.createUserWithEmailAndPassword(email, matkhau).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){ // đăng kí thành công,
-                        Toast.makeText(DangKi_Activity.this, getString(R.string.thongbaodangkithanhcong), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
+        Toast.makeText(this, hoten.trim().length() +"", Toast.LENGTH_SHORT).show();
 
+
+        // kiểm tra điều kiện
+        if(hoten.trim().length() == 0){
+            //thongbaoloinhap += getString(R.string.hoten);
+            Toast.makeText(this, hoten.trim().length(), Toast.LENGTH_SHORT).show(); // xuất thông báo bằng toast
         }
+//        else if(diachi.trim().length() < 10){
+//            thongbaoloinhap += getString(R.string.diachi);
+//            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
+//        }
+//        else if(sdt.trim().length() < 10 || sdt.trim().length() > 10){
+//            thongbaoloinhap += getString(R.string.sodt);
+//            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
+//        }
+//        else if(email.trim().length() == 0){
+//            thongbaoloinhap += getString(R.string.email);
+//            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
+//        }
+//        else if(matkhau.trim().length() == 0){
+//            thongbaoloinhap += getString(R.string.password);
+//            Toast.makeText(this, thongbaoloinhap, Toast.LENGTH_SHORT).show();
+//        }
+//        else if(!nhaplaimk.equals(matkhau)){
+//            Toast.makeText(this, getString(R.string.thongbaonhaplaimk), Toast.LENGTH_SHORT).show();
+//        }
+//        else {
+//            firebaseAuth.createUserWithEmailAndPassword(email, matkhau).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                @Override
+//                public void onComplete(@NonNull Task<AuthResult> task) {
+//                    if(task.isSuccessful()){
+//                        Toast.makeText(DangKi_Activity.this, getString(R.string.thongbaodangkithanhcong), Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
     }
 }
